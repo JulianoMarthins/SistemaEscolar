07643a4bc09707cf24564fa90f5431c964926915
Juliano Martins de Souza <julianopoamartins@gmail.com>
2024-09-14 19:45:40 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaEscolar/Entities/Aluno.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace SistemaEscolar.Entities {
+    internal class Aluno {
+
+        // Atributos
+        public string Nome { get; private set; }
+        private double[] _notas;
+
+
+        // Construtor
+        public Aluno(string nome, int qntProvas) {
+            Nome = nome;
+            _notas = new double[qntProvas];
+        }
+
+
+        // Metodos
+        public double Media {
+            get {
+                return MediaNotas();
+            }
+        }
+
+        public void InserirNotas() {
+            
+            for(int i = 0; i < _notas.Length; i++) {
+                Console.Write($"Digite o valor da {i + 1}° prova: ");
+                _notas[i] = double.Parse(Console.ReadLine());    
+            }
+        }
+
+        private double MediaNotas() {
+
+            double total = 0;
+
+            for(int i = 0; i < _notas.Length; i++) {
+                total += _notas[i];
+            }
+
+            return total / _notas.Length;            
+        }
+
+        
+    }
+}
---
+++ SistemaEscolar/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SistemaEscolar.Entities;
+
+namespace SistemaEscolar {
+    internal class Program {
+        static void Main(string[] args) {
+
+            Console.Title = "Programado por JulianoMarthins";
+
+            Console.Write("Quantos alunos: ");
+            int numAlunos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Aluno[] alunos = new Aluno[numAlunos];
+
+
+            for(int i = 0; i < alunos.Length; i++) {
+                
+                Console.Clear();
+
+                Console.Write($"{i + 1}° Aluno\nDigite seu nome: ");
+                string nome = Console.ReadLine();
+
+                Console.Write($"Quantidade de provas: ");
+                int provas = int.Parse(Console.ReadLine());
+
+                alunos[i] = new Aluno(nome, provas);
+
+                Console.WriteLine($"\nInsira as notas do aluno {nome}: ");
+
+                alunos[i].InserirNotas();
+
+            }
+
+            Console.Clear();
+
+            double mediaGeral = 0;
+
+            foreach(Aluno aluno in alunos) {
+                Console.WriteLine($"Aluno: {aluno.Nome}");
+                Console.WriteLine($"Media: {aluno.Media.ToString("F2")}");
+
+                Console.WriteLine();
+
+                mediaGeral += aluno.Media;
+
+            }
+
+            double resultadoFinal = mediaGeral / alunos.Length;
+
+            Console.WriteLine($"Media geral dos alunos: {resultadoFinal.ToString("F2")}");
+
+
+            
+        }
+    }
+}
